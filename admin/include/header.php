470f424335c33b211c9658433179fae54201f5a7
Pavithra De Silva <56332804+pndesilva@users.noreply.github.com>
2020-01-20 11:28:54 +0530

    Add files via upload

---
+++ admin/include/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	
+<nav class="navbar navbar-default navbar-static-top">
+    <div class="container-fluid">
+	<div class="navbar-header">
+			
+			
+			<a class="navbar-brand" href="#" style="font-size:24px" style="color: red">
+				Admin Panel	</a>		</div>
+
+<div class="collapse navbar-collapse">
+<ul class="nav navbar-nav navbar-right">
+<li class="dropdown ">
+
+        <li><a href="http://localhost/sky/sky/index.php">Logout</a></li>
+	</ul>
+	  </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+	</nav>
+
+
+
+
+</head>
+<body>
+
+</body>
+</html>
+
